4048b3d1f1eb5246b2582a02a8eeb02174c1be10
Kennedy <kgvandel@gmail.com>
2019-04-15 14:18:58 -0700

    Create HW1types.hs

---
+++ HW1types.hs
@@ -0,0 +1,11 @@
+module HW1types where
+
+import Data.List (nub,sort)
+
+type Node  = Int
+type Edge  = (Node,Node)
+type Graph = [Edge]
+type Path  = [Node]
+
+norm :: Ord a => [a] -> [a]
+norm = sort . nub
