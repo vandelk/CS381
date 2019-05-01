a100c856b341b38486ad2b92781d1d89315881ed
Kennedy <kgvandel@gmail.com>
2019-05-01 13:13:44 -0700

    Update assignment3.hs

--- assignment3.hs
+++ assignment3.hs
@@ -1,6 +1,18 @@
 -- Team Members: Kennedy Vandel and Michael Zavalza
 
 --1
+type Prog = [Cmd]
+
+data Cmd = LD Int
+         | ADD
+         | MULT
+         | DUP
+
+type Stack = [Int]
+
+-- sem :: Prog -> D
+--
+-- semCMD :: Cmd -> D
 
 --2a
 
