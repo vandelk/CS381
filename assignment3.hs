2cb03048ec43f5b4a409877bd0d559c86826a634
Kennedy <kgvandel@gmail.com>
2019-05-01 23:38:07 -0700

    changes

--- assignment3.hs
+++ assignment3.hs
@@ -1,4 +1,5 @@
 -- Team Members: Kennedy Vandel and Michael Zavalza
+import SVG
 
 --1
 type Prog = [Cmd]
@@ -9,11 +10,18 @@ data Cmd = LD Int
          | DUP
 
 type Stack = [Int]
+type D = Stack -> Stack
 
--- sem :: Prog -> D
---
--- semCMD :: Cmd -> D
+semCmd :: Cmd -> D
+semCmd (LD i)     x = [i] ++ x
+semCmd ADD  (x:y:z) = [x+y] ++ z
+semCmd MULT (x:y:z) = [x*y] ++ z
+semCmd DUP    (x:y) = [x,x] ++ y
+semCmd _ _ = []
 
+sem :: Prog -> D
+sem []     x = x
+sem (x:xs) i = sem xs (semCmd x i)
 --2a
 
 --2b
@@ -21,3 +29,16 @@ type Stack = [Int]
 --2c
 
 --3
+data CMD = Pen Mode
+         | MoveTo Int Int
+         | Seq CMD CMD
+
+data Mode = Up | Down
+
+type State = (Mode,Int,Int)
+
+semS :: CMD -> State -> (State,Lines)
+semS = undefined
+
+sem' :: CMD ->Lines
+sem' = undefined
