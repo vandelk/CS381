cb1223c6b786b11e8764c92b9a1a8dea5798b6e8
Kennedy <kgvandel@gmail.com>
2019-05-08 12:10:11 -0700

    Create assignment4.hs

---
+++ assignment4.hs
@@ -0,0 +1,13 @@
+-- Team Members: Kennedy Vandel and Michael Zavalza
+
+type Prog = [Cmd]
+
+data Cmd = LD Int
+         | ADD
+         | MULT
+         | DUP
+         | INC
+         | SWAP
+         | POP Int
+
+--1a
