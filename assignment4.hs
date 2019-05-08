5535c5b8b8b787188d15f764c61785d595c3a26e
Kennedy <kgvandel@gmail.com>
2019-05-08 12:14:48 -0700

    Update assignment4.hs

--- assignment4.hs
+++ assignment4.hs
@@ -1,13 +1,25 @@
 -- Team Members: Kennedy Vandel and Michael Zavalza
 
-type Prog = [Cmd]
-
-data Cmd = LD Int
-         | ADD
-         | MULT
-         | DUP
-         | INC
-         | SWAP
-         | POP Int
 
 --1a
+
+
+--1b
+
+
+--2a
+
+
+--2b
+
+
+--3a
+
+
+--3b
+
+
+--3c
+
+
+--3d
