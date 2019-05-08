dda3cf2943fa791a639534786a899b478340cc24
Kennedy <kgvandel@gmail.com>
2019-05-08 12:22:22 -0700

    Update assignment4.hs

--- assignment4.hs
+++ assignment4.hs
@@ -1,17 +1,42 @@
 -- Team Members: Kennedy Vandel and Michael Zavalza
 
+type Prog = [Cmd]
+
+data Cmd = LD Int
+         | ADD
+         | MULT
+         | DUP
+         | INC
+         | SWAP
+         | POP Int
 
 --1a
+type Rank = Int
+type CmdRank = (Int,Int)
 
+rankC :: Cmd -> CmdRank
+rankC = undefined
+
+rankP :: Prog -> Maybe Rank
+rankP = undefined
 
 --1b
 
 
 --2a
+data Shape = X
+           | TD Shape Shape
+           | LR Shape Shape
+           deriving Show
 
+type BBox = (Int, Int)
+
+bbox :: Shape -> BBox
+bbox = undefined
 
 --2b
-
+rect :: Shape -> Maybe BBox
+rect = undefined
 
 --3a
 
